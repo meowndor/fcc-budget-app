90b9931e0c4686793bca32ba7a7b7048d3cadd45
nuluh <dam.ar@outlook.com>
2022-09-23 09:46:50 +0700

    TODO: Spend chart

--- budget.py
+++ budget.py
@@ -1,7 +1,76 @@
 class Category:
+    def __init__(self, label=None):
+        if isinstance(label, str) != True:
+            self.label = str(label)
+        else:
+            self.label = label
 
+        self.ledger = []
 
+    def total_funds(self):
+        funds = 0
+        for i in self.ledger:
+            for key, value in i.items():
+                # print(key, value)
+                if key == "amount":
+                    funds += value
+        return funds
 
+    def check_funds(self, amount=None):
+        if amount <= self.total_funds():
+            return True
+        else:
+            return False
 
+    def get_balance(self):
+        return self.total_funds()
 
-def create_spend_chart(categories):
+    def deposit(self, amount=None, description=""):
+        return self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, expense=None, description=""):
+        if self.check_funds(expense) == True:
+            self.ledger.append(
+                {"amount": -1*expense, "description": description})
+            return True
+        else:
+            return False
+
+    def transfer(self, amount=None, destination=None):
+        if self.check_funds(amount) == True:
+            self.withdraw(amount, f"Transfer to {destination.label}")
+            destination.deposit(amount, f"Transfer from {self.label}")
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        title = self.label.center(30, "*")+"\n"
+        details = ""
+        balance = 0
+        for i in self.ledger:
+            if i["description"] == None or i["amount"] == None:
+                pass
+
+            # TODO: fix the description slicing depends on the length of amount number
+            elif len(i["description"]) > 23:
+                details += i["description"][:23] + \
+                    "{0:.2f}".format(i["amount"]).rjust(
+                        30-len(i["description"][:23])) + "\n"
+            else:
+                details += i["description"] + \
+                    "{0:.2f}".format(i["amount"]).rjust(
+                        30-len(i["description"])) + "\n"
+            balance += i["amount"]
+
+        total = "Total: {0:.2f}".format(balance)
+        # values = []
+        # description = []
+        # for i in self.ledger:
+        #     for key, value in i.items():
+        #         if key == "amount":
+        #             values.append(value)
+        #         elif key == "description":
+        #             description.append(value)
+        return title+details+total
+        # def create_spend_chart(categories):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-from budget import create_spend_chart
+# from budget import create_spend_chart
 from unittest import main
 
 food = budget.Category("Food")
@@ -9,17 +9,20 @@ food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 print(food.get_balance())
 clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
+auto = budget.Category("Auto")
+food.transfer(50, auto)
+print(food.get_balance())
 clothing.withdraw(25.55)
 clothing.withdraw(100)
-auto = budget.Category("Auto")
+
 auto.deposit(1000, "initial deposit")
 auto.withdraw(15)
 
 print(food)
 print(clothing)
+print(auto)
 
-print(create_spend_chart([food, clothing, auto]))
+# print(create_spend_chart([food, clothing, auto]))
 
-# Run unit tests automatically
+# # Run unit tests automatically
 main(module='test_module', exit=False)
